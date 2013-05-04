003646f882dd37d92c240e663a8a917c809b3972
Tyler Treat <ttreat31@gmail.com>
2013-05-04 09:59:11 -0500

    Add Authenticator interface.

---
+++ src/main/java/com/clarionmedia/zinc/messaging/Authenticator.java
@@ -0,0 +1,19 @@
+package com.clarionmedia.zinc.messaging;
+
+/**
+ * Describes how to authenticate a connection request from a client.
+ */
+public interface Authenticator {
+
+    /**
+     * Indicates if the request from the given IP address and port using the provided token is authentic.
+     * If it is, this address will be added as a peer.
+     *
+     * @param ip    the IP address the connection request came from
+     * @param port  the port the connection request came from
+     * @param token the token the connection request sent
+     * @return {@code true} if the request is authentic, {@code false} if not
+     */
+    boolean isAuthenticated(String ip, int port, String token);
+
+}
