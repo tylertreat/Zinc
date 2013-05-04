e7e1f001601c2d161781587b02cd926ce2aaeb0d
Tyler Treat <ttreat31@gmail.com>
2013-05-04 09:49:03 -0500

    Add Peer interface.

---
+++ src/main/java/com/clarionmedia/zinc/messaging/Peer.java
@@ -0,0 +1,24 @@
+package com.clarionmedia.zinc.messaging;
+
+/**
+ * A {@code Peer} is an {@link Endpoint} identified by an address and a port.
+ */
+public class Peer {
+
+    private String address;
+    private int port;
+
+    public Peer(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+}
