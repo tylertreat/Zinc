2381fe6b61840c6ba82b2369b9ebd43efdffc376
Tyler Treat <ttreat31@gmail.com>
2013-05-04 09:43:29 -0500

    Add ClientAddedListener and HostEndpoint.

---
+++ src/main/java/com/clarionmedia/zinc/messaging/ClientAddedListener.java
@@ -0,0 +1,16 @@
+package com.clarionmedia.zinc.messaging;
+
+/**
+ * Event listener that listens for when clients are added to a {@link HostEndpoint}.
+ */
+public interface ClientAddedListener {
+
+    /**
+     * Invoked when a client is added to the {@link HostEndpoint}.
+     *
+     * @param address the client address
+     * @param port the client port
+     */
+    void onClientAdded(String address, int port);
+
+}
---
+++ src/main/java/com/clarionmedia/zinc/messaging/HostEndpoint.java
@@ -0,0 +1,32 @@
+package com.clarionmedia.zinc.messaging;
+
+/**
+ * {@code HostEndpoint} represents the notion of a "host" in a distributed client-host
+ * cluster application.
+ */
+public interface HostEndpoint extends Endpoint {
+
+    /**
+     * Adds the given client, identified by an address and port, to the {@code HostEndpoint}.
+     * This allows the {@code HostEndpoint} to receive messages from it.
+     *
+     * @param address the address of the peer to add
+     * @param port    the port of the peer to add
+     */
+    void addClient(String address, int port);
+
+    /**
+     * Returns the port that's listening for connection requests.
+     *
+     * @return connection request port
+     */
+    int getConnectionRequestPort();
+
+    /**
+     * Adds the {@link ClientAddedListener} to the {@code HostEndpoint}.
+     *
+     * @param listener the {@code ClientAddedListener} to add
+     */
+    void addClientAddedListener(ClientAddedListener listener);
+
+}
