ab172b016b92cd743a6fe15fea0a36f998ad9c31
Tyler Treat <ttreat31@gmail.com>
2013-05-07 22:38:18 -0500

    Add some additional documentation.

--- src/main/java/com/clarionmedia/zinc/endpoint/impl/zmq/ZmqEndpointFactory.java
+++ src/main/java/com/clarionmedia/zinc/endpoint/impl/zmq/ZmqEndpointFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Tyler Treat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.clarionmedia.zinc.endpoint.impl.zmq;
 
 import com.clarionmedia.zinc.endpoint.*;
@@ -8,11 +24,14 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+/**
+ * Implementation of {@link EndpointFactory} using ZeroMQ-based {@code Endpoint} implementations.
+ */
 public class ZmqEndpointFactory implements EndpointFactory {
 
     @Override
     public Endpoint buildClientEndpoint(String hostAddress, int hostDiscoveryPort, String token,
-                                               MessageHandlerFactory messageHandlerFactory) {
+                                        MessageHandlerFactory messageHandlerFactory) {
         int hostSharePort = 0;
         int port = 0;
         Socket socket = null;
@@ -65,8 +84,8 @@ public class ZmqEndpointFactory implements EndpointFactory {
 
     @Override
     public HostEndpoint buildHostEndpoint(int port, MessageHandlerFactory messageHandlerFactory,
-                                                 Authenticator
-            authenticator) {
+                                          Authenticator
+                                                  authenticator) {
         return new ZmqHostEndpoint(port, messageHandlerFactory, authenticator);
     }
 }
